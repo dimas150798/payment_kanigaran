cf9333617d1fca77ee5e7bfcc0e477dfd29b89d3
Dimas Bagus Wahyu Sudrajat <47965622+dimas150798@users.noreply.github.com>
2024-01-23 16:14:15 +0700

    Menampilkan kode DP pada data pelanggan

--- application/views/admin/DataPelanggan/V_DataPelanggan.php
+++ application/views/admin/DataPelanggan/V_DataPelanggan.php
@@ -72,6 +72,7 @@ if (!function_exists('changeDateFormat')) {
                                         <th class="text-center" width="10%">Nama Paket</th>
                                         <th class="text-center" width="10%">Date Registrasi</th>
                                         <th class="text-center" width="15%">sales</th>
+                                        <th class="text-center" width="10%">Kode DP</th>
                                         <th class="text-center" width="10%">Status</th>
                                         <th class="text-center" width="5%">Action</th>
                                     </tr>
